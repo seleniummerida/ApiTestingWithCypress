0377909760726589a0226a32453e34283b7abba0
Jose Canul <selenium.merida@gmail.com>
2022-10-29 23:04:59 -0500

    AddingDelete

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -7,4 +7,30 @@ describe('TODO api testing', () => {
             assert.isArray(todoss.body, 'Todos Response is an array')
         });
     });
+
+    it('Adds Todo item - POST', () => {
+        cy.request('POST', '/todos/', { task: "run tests" }).as('todoRequest');
+        // adds new Todo item by defining Todo name        
+        cy.get('@todoRequest').then(todos => {
+            expect(todos.status).to.eq(200);
+            todoItem = todos.body._id
+            cy.wrap(todos.body).should('deep.include', {
+                task: 'run tests',
+                completed: false,
+            });
+        });
+    });
+
+    
+    
+    it('deletes Todo items - DELETE', () => {
+        cy.request('DELETE', `/todos/${todoItem}`).as('todoRequest');
+        // deletes Todo item with id = 9
+        cy.get('@todoRequest').then(todos => {
+            expect(todos.status).to.eq(200);
+            assert.isString(todos.body, 'todo deleted!')
+        });
+    });
+
  });
+
